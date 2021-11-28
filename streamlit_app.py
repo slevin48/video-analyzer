a1ddf893a2fd1fb8bdbab5369270fc293a911e26
Slevin <yann.debray@gadz.org>
2021-11-28 15:12:38 +0100

    Test streamlit_player on Cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_player import st_player
 import pytube
 import moviepy.editor as mp
 import speech_recognition as sr
@@ -36,18 +37,29 @@ if src == "Upload":
         path = 'downloads/'+up.name
 else:
     url = 'https://www.youtube.com/watch?v=R2nr1uZ8ffc'
-
     url = st.text_input('Youtube URL',url)
 
+    options = {
+        "events": ["onProgress"],
+    }
+
+    event = st_player(url,**options,key="youtube_player")
+    
+    # Select frame
+    select = st.button('Select frame')
+
+    if select:
+        st.write(event.data['playedSeconds'])
+        
     # Download Youtube video
-    youtube = pytube.YouTube(url)
-    videos = youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')
-    l = [s.resolution + " (" + str(round(s.filesize/2**10/2**10)) + " MB)" for s in videos]
-    i = st.radio("Select resolution",range(len(videos)),format_func = lambda x: l[x])
-    video = videos[i]
     dl = st.checkbox('Download')
     
     if dl:   
+        youtube = pytube.YouTube(url)
+        videos = youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')
+        l = [s.resolution + " (" + str(round(s.filesize/2**10/2**10)) + " MB)" for s in videos]
+        i = st.radio("Select resolution",range(len(videos)),format_func = lambda x: l[x])
+        video = videos[i]
         st.write(video)
         title = video.title
         path = video.download('downloads')
